da1da79fdda2537dab843291fd29217528b97339
Vovanda <the.last.of.the.magicians@gmail.com>
2025-04-23 14:06:42 +0800

    Init: базовая структура и конфигурация для микросервиса LLM-service
    Init: the basic structure and configuration for the LLM-service microservice
    
    AVOX-35

---
+++ LLM_service/app/api/routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/health", tags=["Health"])
+async def health_check():
+    """
+    Проверка состояния LLM & Vectorizer сервиса.
+    """
+    return {"status": "ok"}
